7eacf20ea7abd310cd2cc11872e3790c7faeef45
Alex <alex.petrov.snade@gmail.com>
2025-03-31 15:58:39 +0300

    Update deliver-with-econt.php

--- deliver-with-econt.php
+++ deliver-with-econt.php
@@ -115,7 +115,7 @@ use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 if ( class_exists( 'PucFactory' ) ) {
     // The plugin-update-checker library is already loaded.
     $myUpdateChecker = PucFactory::buildUpdateChecker(
-        'https://github.com/econt/woo-plugin', // Official Econt repository
+        'https://github.com/econt/econt-woo-plugin', // Official Econt repository
         __FILE__,
         'deliver-with-econt'
     );
