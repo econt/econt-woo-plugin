2c82d5fa3031817d20e96b6a0ce3e77df6f83c95
Emichaaa <emichaaa@gmail.com>
2025-04-10 01:26:09 +0300

    Version 2.5.61 - Update BG system translates.

--- deliver-with-econt.php
+++ deliver-with-econt.php
@@ -13,7 +13,7 @@
  * Plugin URI:        https://econt.com/developers/
  * Description:       Econt Shipping Module
  * Version:           2.5.61
- * Author:            Econt Express LTD.
+ * Author:            Econt Express LTD
  * Author URI:        https://econt.com/developers/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
